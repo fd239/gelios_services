6c53d100e37aa48d7f5893681b6a899b73bd5769
Фролов Дмитрий Алексеевич <frolovda@ins.ru>
2020-08-04 11:09:31 +0300

    auto_update

--- gelios_services/passport_check/urls.py
+++ gelios_services/passport_check/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import passport_update
+from .views import passport_manual_update
 
 urlpatterns = [
-    path('passport_update/', passport_update, name='passport_update'),
+    path('passport_update/', passport_manual_update, name='passport_update'),
 ]
--- gelios_services/passport_check/views.py
+++ gelios_services/passport_check/views.py
@@ -1,22 +1,50 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.core.files.storage import default_storage
 from django.http import HttpResponse, Http404
 from django.shortcuts import redirect, render
 
 from .forms import FilePathForm
+from .models import Passport
+
+import csv
+import os
+import urllib2
+import bz2
+
+PASSPORT_LIST_URL = 'http://guvm.mvd.ru/upload/expired-passports/list_of_expired_passports.csv.bz2'
 
 
+def passport_manual_update(request):
 
-# @login_required
-def passport_update(request):
-    
     if request.method == 'POST':
         form = FilePathForm(request.POST)
         if form.is_valid():
-            return render(request, 'passport_update.html', {'form': form})
-
+            load_passporsts(form.cleaned_data['file_path'])
+            return render(request, 'passport_update.html', {'form': form, 'updated': True})
     else:
         form = FilePathForm()
 
-    return render(request, 'passport_update.html', {'form': form})
+    return render(request, 'passport_update.html', {'form': form, 'updated': False})
 
+
+def passport_auto_update(request):
+
+    # TODO переделать здесь всё
+    passport_file = urllib2.urlopen(PASSPORT_LIST_URL)
+    with open('passport_file.csv', 'wb') as output:
+        output.write(mp3file.read())
+
+    with bz2.open("myfile.bz2", "wb") as f:
+        load_passporsts(f)
+
+
+def load_passporsts(file_path):
+
+    Passport.objects.all().delete()
+
+    with open(file_path) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            NewPassport = Passport.objects.create(series=row[0], number=row[1])
+            NewPassport.save()
